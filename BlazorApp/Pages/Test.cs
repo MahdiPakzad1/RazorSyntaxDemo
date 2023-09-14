f2502870cc13bec22142c54a4f9a3747a7a71f6f
Mahdi Pakzad <elias-developer@outlook.com>
2023-09-14 19:09:34 +0330

    Add project files.

---
+++ BlazorApp/Pages/Test.cs
@@ -0,0 +1,14 @@
+﻿using BlazorApp.Data;
+
+namespace BlazorApp.Pages
+{
+    public class Test
+    {
+        private readonly WeatherForecastService _weatherForecastService;
+
+        public Test(WeatherForecastService weatherForecastService )
+        {
+            _weatherForecastService = weatherForecastService;
+        }
+    }
+}
